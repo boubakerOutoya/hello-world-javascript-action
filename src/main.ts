df41f00c5b69cd2cd627a10104b8ae7507b7cfbb
outoya <outoya.boubaker@gmail.com>
2020-01-25 23:53:12 +0100

    fix: import module issue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import * as exec from "@actions/exec";
-import * as io from "@actions/io";
 
 async function run(): Promise<void> {
   try {
